855f70cc66e04d941b7f92587f698e991fd9ed95
shaheer <72137242+mshaheerz@users.noreply.github.com>
2021-05-07 09:08:22 +0530

    Create fix1.sh

---
+++ fix1.sh
@@ -0,0 +1,1 @@
+mkdir -p $PREFIX/etc/apt/sources.list.d echo 'deb https://ipfs.io/ipfs/QmVAkuhfFfLBeTKCYYXWBvb3WxgZqDvd8qd7cvQjyrSvhr stable main' \ > $PREFIX/etc/apt/sources.list.d/temp_outage.list
